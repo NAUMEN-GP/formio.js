3020ea93a0482923cc63919ad99feead10ce4d93
phudyshkin <laanser@gmail.com>
2017-12-18 12:00:46 +0500

    Fix Email validation

--- build/components/Validator.js
+++ build/components/Validator.js
@@ -165,7 +165,7 @@ var Validator = exports.Validator = {
       check: function check(component, setting, value) {
         // From http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(value);
+        return component.isEmpty(value) || re.test(value);
       }
     },
     date: {
